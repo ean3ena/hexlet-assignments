25820798256a7d3ace6aba0137ff577d7f711daf
@hexlet/cli <support@hexlet.io>
2024-10-05 18:42:15 +0300

    submit java-oop-ru/errors

---
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -0,0 +1,26 @@
+package exercise;
+
+// BEGIN
+public class Circle {
+
+    private static final double PI = 3.1415;
+    private Point point;
+    private int radius;
+
+    public Circle(Point point, int radius) {
+        this.point = point;
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public double getSquare() throws NegativeRadiusException {
+        if (radius < 0) {
+            throw new NegativeRadiusException("Не удалось посчитать площадь");
+        }
+        return PI * radius * radius;
+    }
+}
+// END
