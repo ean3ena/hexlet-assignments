212a211376ad069ce688faafa08df7aaa9ed95d2
@hexlet/cli <support@hexlet.io>
2025-02-02 13:04:24 +0300

    submit java-spring-boot-ru/service-layer

---
+++ java-spring-boot-ru/service-layer/src/main/java/exercise/controller/BooksController.java
@@ -0,0 +1,60 @@
+package exercise.controller;
+
+import java.util.List;
+
+import exercise.dto.BookCreateDTO;
+import exercise.dto.BookDTO;
+import exercise.dto.BookUpdateDTO;
+import exercise.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/books")
+public class BooksController {
+    @Autowired
+    private BookService bookService;
+
+    // BEGIN
+    @GetMapping("")
+    public List<BookDTO> index() {
+        var booksDTO = bookService.getAll();
+        return booksDTO;
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO show(@PathVariable Long id) {
+        var bookDTO = bookService.findById(id);
+        return bookDTO;
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDTO create(@RequestBody @Valid BookCreateDTO bookData) {
+        var bookDTO = bookService.create(bookData);
+        return bookDTO;
+    }
+
+    @PutMapping("/{id}")
+    public BookDTO update(@RequestBody @Valid BookUpdateDTO bookData, @PathVariable Long id) {
+        var bookDTO = bookService.update(bookData, id);
+        return bookDTO;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        bookService.delete(id);
+    }
+    // END
+}
